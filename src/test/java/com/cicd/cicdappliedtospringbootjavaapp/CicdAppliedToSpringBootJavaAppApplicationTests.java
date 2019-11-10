f1749bd719d1546556c2f91a6263fde34d05d0ce
Denis Tupichkin <denis.tupichkin@white-soft.ru>
2019-11-10 14:42:03 +1000

    Add controller test

--- src/test/java/com/cicd/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplicationTests.java
+++ src/test/java/com/cicd/cicdappliedtospringbootjavaapp/CicdAppliedToSpringBootJavaAppApplicationTests.java
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CicdAppliedToSpringBootJavaAppApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
---
+++ src/test/java/com/cicd/cicdappliedtospringbootjavaapp/controller/HelloControllerTest.java
@@ -0,0 +1,20 @@
+package com.cicd.cicdappliedtospringbootjavaapp.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HelloControllerTest {
+
+    @Autowired
+    private HelloController helloController;
+
+    @Test
+    void home() {
+        assertThat(helloController.home()).isEqualToIgnoringCase("Hello world...");
+    }
+}
