0c98196031c7be96c5c6ddeac60c296a89b77d2d
Denis Tupichkin <denis.tupichkin@white-soft.ru>
2019-11-10 16:32:13 +1000

    fix error

--- src/main/java/com/cicd/cicdappliedtospringbootjavaapp/controller/HelloController.java
+++ src/main/java/com/cicd/cicdappliedtospringbootjavaapp/controller/HelloController.java
@@ -5,9 +5,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    
     @GetMapping ("/")
     public String home(){
-        return "Hello world..."+
+        return "Hello world...";
     }
 
     @GetMapping ("/good-by")
